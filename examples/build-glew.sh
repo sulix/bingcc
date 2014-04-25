fca5b9b40e6724b82ec8090c25a8f92405937727
David Gow <david@ingeniumdigital.com>
2014-04-25 10:35:40 +0800

    Fixes + glew builder.

---
+++ examples/build-glew.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+GLEWVER=1.10.0
+
+# Get the bingcc path: feel free to replace this with a hardcoded path.
+MYFILENAME=`readlink -f "$0"`
+MYPATH=`dirname "$MYFILENAME"`
+BINGCCPATH=`readlink -f "$MYPATH/.."`
+
+wget -c http://downloads.sourceforge.net/project/glew/glew/$GLEWVER/glew-$GLEWVER.tgz
+tar xf glew-$GLEWVER.tgz
+
+BASEPATH=`readlink -f .`
+mkdir -p libs-32
+mkdir -p libs
+
+cd glew-$GLEWVER
+# Cross-compile a 32-bit version
+
+CC=$BINGCCPATH/bingcc32 make GLEW_DEST="$BASEPATH/libs-32" install
+
+cd ..
+
+# Damn: zlib can't do out-of-tree builds.
+rm -rf glew-$GLEWVER
+
+tar xf glew-$GLEWVER.tgz
+cd glew-$GLEWVER
+# Compile a 64-bit version
+CC=$BINGCCPATH/bingcc make GLEW_DEST="$BASEPATH/libs" install
--- examples/build-sdl2.sh
+++ examples/build-sdl2.sh
@@ -24,8 +24,8 @@ mkdir -p build-32
 cd build-32
 
 #CC=$BINGCCPATH/bingcc32 cmake .. -DCMAKE_INSTALL_PREFIX="$BASEPATH/libs-32" -DRPATH=0 -DPULSEAUDIO=1 -DPULSEAUDIO_SHARED=1 -DALSA=1 -DALSA_SHARED=1 -DESD=0 -DARTS=0 -DSDL_DLOPEN=1 -DX11_SHARED=1 -DVIDEO_OPENGL=1 -DVIDEO_OPENGLES=0
-CC=$BINGCCPATH/bingcc32 ../configure --prefix="$BASEPATH/libs-32" --disable-esd --disable-arts --disable-video-directfb --disable-rpath --enable-alsa --enable-alsa-shared --enable-pulseaudio --enable-pulseaudio-shared --enable-x11-shared --enable-sdl-dlopen --disable-video-opengles
-make install
+CC=$BINGCCPATH/bingcc32 ../configure --prefix="$BASEPATH/libs-32" --disable-esd --disable-arts --disable-video-directfb --disable-rpath --enable-alsa --enable-alsa-shared --enable-pulseaudio --enable-pulseaudio-shared --enable-x11-shared --enable-sdl-dlopen --disable-input-tslib
+make -j9 install
 
 cd ..
 
@@ -36,6 +36,6 @@ mkdir -p build
 cd build
 
 #CC=$BINGCCPATH/bingcc cmake .. -DCMAKE_INSTALL_PREFIX="$BASEPATH/libs" -DRPATH=0 -DPULSEAUDIO=1 -DPULSEAUDIO_SHARED=1 -DALSA=1 -DALSA_SHARED=1 -DESD=0 -DARTS=0 -DSDL_DLOPEN=1 -DX11_SHARED=1 -DVIDEO_OPENGL=1 -DVIDEO_OPENGLES=0
-CC=$BINGCCPATH/bingcc ../configure --prefix="$BASEPATH/libs" --disable-esd --disable-arts --disable-video-directfb --disable-rpath --enable-alsa --enable-alsa-shared --enable-pulseaudio --enable-pulseaudio-shared --enable-x11-shared --enable-sdl-dlopen --disable-video-opengles
-make install
+CC=$BINGCCPATH/bingcc ../configure --prefix="$BASEPATH/libs" --disable-esd --disable-arts --disable-video-directfb --disable-rpath --enable-alsa --enable-alsa-shared --enable-pulseaudio --enable-pulseaudio-shared --enable-x11-shared --enable-sdl-dlopen --disable-input-tslib
+make -j9 install
 
