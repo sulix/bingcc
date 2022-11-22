fc1ac26c73ef788e83ad98d3cca1b55b1f150f20
David Gow <david@ingeniumdigital.com>
2022-11-22 16:32:22 +0800

    examples: Update build-sdl2 in preparation for SDL3
    We can't just assume git HEAD is SDL2 anymore.
    
    SDL3 will probably use CMake, as well, so will be quite different.
    
    Also use `nproc` rather than a hardcoded 9 threads.

--- examples/build-sdl2.sh
+++ examples/build-sdl2.sh
@@ -6,7 +6,7 @@ MYFILENAME=`readlink -f "$0"`
 MYPATH=`dirname "$MYFILENAME"`
 BINGCCPATH=`readlink -f "$MYPATH/.."`
 
-git clone https://github.com/libsdl-org/SDL.git
+git clone --depth 1 --branch SDL2 https://github.com/libsdl-org/SDL.git SDL2
 
 #wget -O fix-fullscreen.patch "http://bugzilla-attachments.libsdl.org/attachment.cgi?id=1059"
 
@@ -14,7 +14,7 @@ BASEPATH=`readlink -f .`
 mkdir -p libs-32
 mkdir -p libs
 
-cd SDL
+cd SDL2
 # Cross-compile a 32-bit version
 
 mkdir -p build-32
@@ -23,7 +23,7 @@ cd build-32
 
 #CC=$BINGCCPATH/bingcc32 cmake .. -DCMAKE_INSTALL_PREFIX="$BASEPATH/libs-32" -DRPATH=0 -DPULSEAUDIO=1 -DPULSEAUDIO_SHARED=1 -DALSA=1 -DALSA_SHARED=1 -DESD=0 -DARTS=0 -DSDL_DLOPEN=1 -DX11_SHARED=1 -DVIDEO_OPENGL=1 -DVIDEO_OPENGLES=0
 CC=$BINGCCPATH/bingcc32 ../configure --prefix="$BASEPATH/libs-32" --disable-esd --disable-arts --disable-video-directfb --disable-rpath --enable-alsa --enable-alsa-shared --enable-pulseaudio --enable-pulseaudio-shared --enable-x11-shared --enable-sdl-dlopen --disable-input-tslib
-make -j9 install
+make -j`nproc` install
 
 cd ..
 
@@ -35,5 +35,5 @@ cd build
 
 #CC=$BINGCCPATH/bingcc cmake .. -DCMAKE_INSTALL_PREFIX="$BASEPATH/libs" -DRPATH=0 -DPULSEAUDIO=1 -DPULSEAUDIO_SHARED=1 -DALSA=1 -DALSA_SHARED=1 -DESD=0 -DARTS=0 -DSDL_DLOPEN=1 -DX11_SHARED=1 -DVIDEO_OPENGL=1 -DVIDEO_OPENGLES=0
 CC=$BINGCCPATH/bingcc ../configure --prefix="$BASEPATH/libs" --disable-esd --disable-arts --disable-video-directfb --disable-rpath --enable-alsa --enable-alsa-shared --enable-pulseaudio --enable-pulseaudio-shared --enable-x11-shared --enable-sdl-dlopen --disable-input-tslib
-make -j9 install
+make -j`nproc` install
 
