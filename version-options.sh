f635db2fe9009ce5970415152fecfd1913c9293a
David Gow <david@ingeniumdigital.com>
2023-04-24 12:05:43 +0800

    Rebuild
    Rebuild everything against glibc 2.35 headers, targeting 2.14.
    
    This includes the fcntl64 bits.

--- version-options.sh
+++ version-options.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-bingcc_veropts="-Wl,$bingcc_incpath/glibc_wrapper.o"
+bingcc_veropts="-Wl,--wrap=fcntl64 "
+bingcc_veropts+="-Wl,$bingcc_incpath/glibc_wrapper.o"
--- version-options32.sh
+++ version-options32.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-bingcc_veropts="-m32 -Wl,$bingcc_incpath/glibc_wrapper32.o -mstackrealign"
+bingcc_veropts="-Wl,--wrap=fcntl64 "
+bingcc_veropts+="-m32 -Wl,$bingcc_incpath/glibc_wrapper32.o"
